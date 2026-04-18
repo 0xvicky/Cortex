eb78229c588f4e0ad26cfc220bc3f22f28c43503
vicky <tyagivivek528@gmail.com>
2026-04-18 13:04:25 +0530

    removed unwanted files

--- internal/controller/prcs_controller.go
+++ internal/controller/prcs_controller.go
@@ -5,6 +5,7 @@ import (
 	"cortex/internal/model"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -49,9 +50,18 @@ func AnalyzeRepoHandler(c *gin.Context) {
 			zap.Error(pathErr),
 		)
 	}
+	//2. Cloning the project in path
+	cmd := exec.Command("git", "clone", req.RepoLink, path)
+	if cmdErr := cmd.Run(); cmdErr != nil {
+		l.Log.Error("error while cloning repo",
+			zap.Error(cmdErr),
+		)
+
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"message": "Pong",
+		"message": "clone success",
 		"path":    path,
 	})
 
