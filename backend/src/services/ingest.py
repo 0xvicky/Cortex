0db34cbf3b27f1337326ec0d506beebf707196fe
vicky <tyagivivek528@gmail.com>
2026-05-02 14:47:32 +0530

    cloned repo into external temp

--- backend/src/services/ingest.py
+++ backend/src/services/ingest.py
@@ -3,6 +3,7 @@ from fastapi import UploadFile, HTTPException
 from pydantic import HttpUrl
 from urllib.parse import urlparse
 from github import Github, UnknownObjectException
+from git import Repo
 from dotenv import load_dotenv
 import httpx
 import os
@@ -28,9 +29,18 @@ async def ingestr_svc(repo_url: str):
 
     # create a local temp folder with unique name to store clone repo
     PATH = "Z:/Code/Golang/Projects/cortex/temp"
+    # generated unique id
     repo_name = uuid.uuid8()
     # clone that repo into that folder
-    print(repo_name)
-    repo_path = Path(f"{PATH}/{repo_name}")
-    repo_path.mkdir(parents=True, exist_ok=True)
-    return {"repoUrl": repo_path}
+    # print(repo_name)
+    local_path = Path(f"{PATH}/{repo_name}")
+    # created the repo folder is not exist with parents
+    local_path.mkdir(parents=True, exist_ok=True)
+
+    cloned_repo = Repo.clone_from(repo_url, local_path)
+    print(cloned_repo)
+    return {"repoUrl": local_path}
+
+
+# https://github.com/0xvicky/nebula
+# https://github.com/0xvicky/temp-conv-rust
